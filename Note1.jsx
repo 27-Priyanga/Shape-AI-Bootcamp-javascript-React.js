7a404aa864bfeb8a8096042564b7f3f1013c5878
27-Priyanga <85440307+27-Priyanga@users.noreply.github.com>
2021-06-06 17:33:51 +0530

    Add files via upload

---
+++ Note1.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+
+function Note1() {
+  return (
+    <div className="note">
+      <h1> Python</h1>
+      <p>
+        {" "}
+        Python is an interpreted high-level general-purpose programming
+        language. Python's design philosophy emphasizes code readability with
+        its notable use of significant indentation
+      </p>
+    </div>
+  );
+}
+export default Note1;
